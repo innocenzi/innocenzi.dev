cb85e1e84c22c276a1354fc6982d764d794c062d
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-30 12:40:46 +0200

    style: format files

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,18 @@
 <template>
-  <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-  </Layout>
+	<Layout>
+		<h1>About us</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a
+			voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime!
+			Iusto.
+		</p>
+	</Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'About us'
-  }
-}
+	metaInfo: {
+		title: 'About us',
+	},
+};
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,32 @@
 <template>
-  <Layout>
+	<Layout>
+		<!-- Learn how to use images here: https://gridsome.org/docs/images -->
+		<g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+		<h1>Hello, world!</h1>
 
-    <h1>Hello, world!</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste
+			tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+		</p>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+		<p class="home-links">
+			<a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+			<a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+		</p>
+	</Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+	metaInfo: {
+		title: 'Hello, world!',
+	},
+};
 </script>
 
 <style>
 .home-links a {
-  margin-right: 1rem;
+	margin-right: 1rem;
 }
 </style>
