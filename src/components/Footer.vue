cd62cf337b389b6357fe5a6f6c38bf735a2a8426
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-30 20:44:28 +0200

    chore: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+	<footer class="flex items-center justify-center opacity-75">
+		<span class="inline-flex">
+			Made with
+			<icon-heart class="w-6 mx-2 text-brand" />
+			by
+			<span class="font-bold">&nbsp;Enzo Innocenzi&nbsp;</span>
+			thanks to
+			<a href="https://gridsome.org/" class="font-bold transition-colors duration-150 hover:text-brand">
+				&nbsp;Gridsome
+			</a>
+		</span>
+	</footer>
+</template>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,21 @@
 <template>
-	<main class="h-full min-h-screen py-12 antialiased bg-surface text-on-background">
+	<main class="flex flex-col h-full min-h-screen py-12 antialiased bg-surface text-on-background">
 		<page-header class="container mx-auto md:px-16" />
-		<section class="container mx-auto mt-16 md:px-16">
+		<section class="container flex-1 mx-auto mt-16 md:px-16">
 			<slot />
 		</section>
+		<page-footer class="container mx-auto mt-40 md:px-16" />
 	</main>
 </template>
 
 <script>
 import PageHeader from '~/components/Header.vue';
+import PageFooter from '~/components/Footer.vue';
 
 export default {
 	components: {
 		PageHeader,
+		PageFooter,
 	},
 };
 </script>
