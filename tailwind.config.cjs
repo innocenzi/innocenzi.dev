4b3b0ea219990ef41656039438b6e6099376c285
Enzo Innocenzi <enzo@innocenzi.dev>
2022-10-04 16:50:15 +0200

    chore: improve typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
+				sans: ['InterVariable', 'ui-sans-serif', 'system-ui', '-apple-system'],
 				karla: ['KarlaVariable', 'ui-sans-serif', 'system-ui', '-apple-system'],
 			},
 		},
