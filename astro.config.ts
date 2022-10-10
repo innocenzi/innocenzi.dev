4a734d30d395eaf3fc29e8950678f710c673c393
Enzo Innocenzi <enzo@innocenzi.dev>
2022-10-10 20:10:33 +0200

    feat: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,20 @@
 import path from 'node:path'
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 import vue from '@astrojs/vue'
 import image from '@astrojs/image'
 import prefetch from '@astrojs/prefetch'
 import icons from 'unplugin-icons/vite'
 
 export default defineConfig({
+	site: 'https://innocenzi.dev',
 	integrations: [
 		tailwind(),
 		vue(),
 		image(),
 		prefetch(),
+		sitemap(),
 	],
 	markdown: {
 		shikiConfig: {
