fe17e78c3e09e2590a0505e1aa0550ee59945249
Enzo Innocenzi <enzo@innocenzi.dev>
2023-03-04 17:44:56 +0100

    chore: update stack

---
+++ components/global/hybridly.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+	<svg width="171" height="175" viewBox="0 0 171 175" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M110.513 114.299C126.361 103.699 137.758 77.6936 123.495 52.6673L85.4646 0C70.5395 14.1832 64.7851 25.9279 66.4141 42.0769C67.4903 52.7463 73.002 62.3344 79.2785 71.0293L110.513 114.299Z" fill="url(#paint0_linear_514_82)" />
+		<path d="M84.3662 174.811C100.215 164.211 111.612 138.205 97.348 113.179L59.3178 60.5114C44.3926 74.6946 38.6382 86.4393 40.2672 102.588C41.3435 113.258 46.8552 122.846 53.1316 131.541L84.3662 174.811Z" fill="url(#paint1_linear_514_82)" />
+		<defs>
+			<linearGradient
+				id="paint0_linear_514_82"
+				x1="98.0734"
+				y1="0"
+				x2="98.0734"
+				y2="114.299"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="currentColor" />
+				<stop offset="1" stop-color="currentColor" />
+			</linearGradient>
+			<linearGradient
+				id="paint1_linear_514_82"
+				x1="71.9266"
+				y1="60.5114"
+				x2="71.9266"
+				y2="174.811"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="currentColor" />
+				<stop offset="1" stop-color="currentColor" />
+			</linearGradient>
+		</defs>
+	</svg>
+</template>
