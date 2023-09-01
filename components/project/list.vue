1baee7e64e977d0b5e5950267e05aa48ce9051b9
Enzo Innocenzi <enzo@innocenzi.dev>
2023-09-01 03:52:25 +0200

    feat: add small-scope projects

--- components/project/list.vue
+++ components/project/list.vue
@@ -1,51 +1,81 @@
 <script setup lang="ts">
 interface Project {
-	icon_url: string
+	main: boolean
+	deprecated?: boolean
+	icon_url?: string
 	repository_url: string
-	documentation_url: string
+	documentation_url?: string
+	site_url?: string
 	title: string
 	description: string
 	created_at: string
 }
 
-defineProps<{
+const $props = defineProps<{
 	projects: Project[]
+	main: boolean
 }>()
 
-function sortByCreationDate(projects: Project[]) {
-	return projects.sort((a, b) => {
-		return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-	})
-}
+const projects = computed(() => {
+	return $props.projects
+		.slice()
+		.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+		.filter((project) => project.main === $props.main)
+})
 </script>
 
 <template>
-	<div class="mt-16 sm:mt-20">
-		<ul role="list" class="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
-			<li v-for="project in sortByCreationDate(projects)" :key="project.title" class="group relative flex flex-col items-start">
-				<div class="relative z-10 flex h-12 w-12 items-center justify-center rounded-full border border-zinc-700/50 bg-zinc-800 p-1.5 shadow-md ring-0">
-					<img
-						:alt="`Icon for the ${project.title} project`"
-						:src="project.icon_url"
-						class="h-full w-full"
-					/>
+	<ul role="list" class="grid grid-cols-1 gap-x-12 gap-y-8 sm:gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
+		<li
+			v-for="project in projects"
+			:key="project.title"
+			class="group relative flex flex-col items-start"
+		>
+			<!-- Icon -->
+			<div v-if="project.icon_url" class="relative z-10 flex h-12 w-12 items-center justify-center rounded-full border border-zinc-700/50 bg-zinc-800 p-1.5 shadow-md ring-0">
+				<img
+					:alt="`Icon for the ${project.title} project`"
+					:src="project.icon_url"
+					class="h-full w-full"
+				/>
+			</div>
+			<a :href="project.repository_url" class="hover:bg-zinc-800/20 transition mt-0.5 sm:mt-4 px-4 py-2 sm:py-3 -mx-4 -my-1 sm:-my-3 rounded-md group">
+				<!-- Project name -->
+				<div class="flex items-center justify-between">
+					<h2 class="text-base font-semibold text-zinc-200 group-hover:text-zinc-100 transition" v-text="project.title" />
+					<span
+						v-if="project.deprecated"
+						class="inline-flex opacity-70 items-center rounded-md bg-orange-400/10 px-2 py-1 text-xs font-medium text-orange-200 ring-1 ring-inset ring-orange-400/20"
+						title="This project is no longer maintained or relevant"
+					>
+						deprecated
+					</span>
 				</div>
-				<h2 class="mt-6 text-base font-semibold text-zinc-100">
-					<a class="relative z-10 decoration-dotted hover:underline" :href="project.repository_url" v-text="project.title" />
-				</h2>
-				<p class="relative z-10 mt-2 text-sm text-zinc-400">
-					{{ project.description }}
-				</p>
+				<!-- Description -->
+				<p class="relative z-10 mt-2 text-sm text-zinc-400 group-hover:text-zinc-200 transition-colors" v-text="project.description" />
 				<span class="grow" />
-				<div class="mt-6 flex items-center gap-x-4">
-					<a :href="project.repository_url" class="text-zinc-500 transition hover:text-zinc-300" title="Repository">
+				<!-- Links -->
+				<div
+					class="flex sm:items-center gap-x-4 sm:gap-x-2 gap-y-1 -ml-0.5"
+					:class="{
+						'mt-3 sm:mt-6': project.main,
+						'mt-3 sm:mt-3': !project.main,
+					}"
+				>
+					<a :href="project.repository_url" class="flex items-center text-zinc-500 transition hover:text-zinc-300" title="Repository" @click.stop>
 						<icon name="mdi:github" class="h-6 w-6" />
+						<span class="sm:hidden ml-1.5 text-sm">GitHub</span>
 					</a>
-					<a :href="project.documentation_url" class="text-zinc-500 transition hover:text-zinc-300" title="Documentation">
+					<a v-if="project.documentation_url" :href="project.documentation_url" class="flex items-center text-zinc-500 transition hover:text-zinc-300" title="Documentation" @click.stop>
 						<icon name="mdi:document" class="h-6 w-6" />
+						<span class="sm:hidden ml-1.5 text-sm">Documentation</span>
+					</a>
+					<a v-if="project.site_url" :href="project.site_url" class="flex items-center text-zinc-500 transition hover:text-zinc-300" title="Website" @click.stop>
+						<icon name="mdi:web" class="h-6 w-6" />
+						<span class="sm:hidden ml-1.5 text-sm">Website</span>
 					</a>
 				</div>
-			</li>
-		</ul>
-	</div>
+			</a>
+		</li>
+	</ul>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -17,12 +17,23 @@ definePageMeta({
 
 <template>
 	<main>
+		<!-- Main projects -->
 		<header class="max-w-2xl">
 			<h1 class="text-3xl font-bold tracking-tight text-zinc-100 sm:text-5xl sm:leading-tight md:text-4xl">
 				I've made a few things.
 			</h1>
 			<p class="mt-4" v-text="description" />
 		</header>
-		<project-list :projects="projects" />
+		<project-list class="mt-16 sm:mt-20" :projects="projects" :main="true" />
+		<!-- Other projects -->
+		<div class="max-w-2xl mt-20 sm:mt-32">
+			<h2 class="text-xl font-bold tracking-tight text-zinc-100 sm:text-5xl sm:leading-tight md:text-2xl">
+				Other notable projects
+			</h2>
+			<p class="mt-2">
+				These are smaller-scope projects that may be worth mentioning.
+			</p>
+		</div>
+		<project-list class="mt-10 sm:mt-6" :projects="projects" :main="false" />
 	</main>
 </template>
