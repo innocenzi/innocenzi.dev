0b9b09b65ffde7a232e7601ac2527be82ba5711d
Enzo Innocenzi <enzo@innocenzi.dev>
2025-11-27 18:55:46 +0100

    chore: initialize project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+
+	modules: [
+		'@nuxt/content',
+		'@nuxt/hints',
+		'@nuxt/image',
+		'@nuxt/ui',
+	],
+})
