4e5f69c2e0aa6a623e787cf90508c56530cdf750
Enzo Innocenzi <enzo@innocenzi.dev>
2026-01-09 19:01:19 +0100

    fix: flash of unstyled content on slow network

--- app/components/prose-link.vue
+++ app/components/prose-link.vue
@@ -10,13 +10,15 @@ defineProps<{
 <template>
 	<ULink class="group inline-flex items-center gap-x-1.25 text-highlighted" :href>
 		<slot>{{ text }}</slot>
-		<div v-if="icon && external" class="relative size-4.5">
-			<UIcon :name="icon" class="absolute group-hover:opacity-0 size-full group-hover:scale-80 transition delay-75 group-hover:delay-0" />
-			<UIcon
-				name="tabler:external-link"
-				class="absolute opacity-0 group-hover:opacity-100 size-full scale-80 group-hover:scale-100 transition delay-0 group-hover:delay-75"
-			/>
-		</div>
-		<UIcon v-else-if="external === true" name="tabler:external-link" />
+		<span class="inline-flex relative items-center size-4.5">
+			<template v-if="icon && external">
+				<UIcon :name="icon" class="absolute group-hover:opacity-0 size-full group-hover:scale-80 transition delay-75 group-hover:delay-0" />
+				<UIcon
+					name="tabler:external-link"
+					class="absolute opacity-0 group-hover:opacity-100 size-full scale-80 group-hover:scale-100 transition delay-0 group-hover:delay-75"
+				/>
+			</template>
+			<UIcon v-else-if="external === true" name="tabler:external-link" />
+		</span>
 	</ULink>
 </template>
